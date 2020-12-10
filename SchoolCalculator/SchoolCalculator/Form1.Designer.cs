8483ed9fd1b1789c9520ebbdb2f0c1d5f04fde64
Anna <anna.h.hallberg.92@gmail.com>
2020-12-10 15:17:17 +0100

    Create form

--- SchoolCalculator/SchoolCalculator/Form1.Designer.cs
+++ SchoolCalculator/SchoolCalculator/Form1.Designer.cs
@@ -29,10 +29,21 @@ namespace SchoolCalculator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(502, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SchoolCalculator";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
