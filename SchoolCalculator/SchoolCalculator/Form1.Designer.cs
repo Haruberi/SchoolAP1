9e8d57e7444918e189e9492481a8313163886e8e
Anna <anna.h.hallberg.92@gmail.com>
2020-12-11 11:03:36 +0100

    Create functions with comma

--- SchoolCalculator/SchoolCalculator/Form1.Designer.cs
+++ SchoolCalculator/SchoolCalculator/Form1.Designer.cs
@@ -32,23 +32,23 @@ namespace SchoolCalculator
             this.numberOne = new System.Windows.Forms.Button();
             this.numberTwo = new System.Windows.Forms.Button();
             this.numberZero = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.divButton = new System.Windows.Forms.Button();
             this.numberNine = new System.Windows.Forms.Button();
             this.numberEight = new System.Windows.Forms.Button();
             this.numberSeven = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
+            this.plusButton = new System.Windows.Forms.Button();
             this.numberSix = new System.Windows.Forms.Button();
             this.numberFive = new System.Windows.Forms.Button();
             this.numberFour = new System.Windows.Forms.Button();
             this.numberThree = new System.Windows.Forms.Button();
-            this.button14 = new System.Windows.Forms.Button();
-            this.button15 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.equalButton = new System.Windows.Forms.Button();
+            this.commaButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.multiButton = new System.Windows.Forms.Button();
+            this.ceButton = new System.Windows.Forms.Button();
+            this.cButton = new System.Windows.Forms.Button();
             this.ResultBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.showOperationLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // numberOne
@@ -84,15 +84,16 @@ namespace SchoolCalculator
             this.numberZero.UseVisualStyleBackColor = true;
             this.numberZero.Click += new System.EventHandler(this.button_click);
             // 
-            // button4
+            // divButton
             // 
-            this.button4.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(257, 149);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(70, 70);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "/";
-            this.button4.UseVisualStyleBackColor = true;
+            this.divButton.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divButton.Location = new System.Drawing.Point(257, 149);
+            this.divButton.Name = "divButton";
+            this.divButton.Size = new System.Drawing.Size(70, 70);
+            this.divButton.TabIndex = 7;
+            this.divButton.Text = "/";
+            this.divButton.UseVisualStyleBackColor = true;
+            this.divButton.Click += new System.EventHandler(this.operator_click);
             // 
             // numberNine
             // 
@@ -127,15 +128,16 @@ namespace SchoolCalculator
             this.numberSeven.UseVisualStyleBackColor = true;
             this.numberSeven.Click += new System.EventHandler(this.button_click);
             // 
-            // button8
+            // plusButton
             // 
-            this.button8.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(257, 374);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(70, 70);
-            this.button8.TabIndex = 11;
-            this.button8.Text = "+";
-            this.button8.UseVisualStyleBackColor = true;
+            this.plusButton.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusButton.Location = new System.Drawing.Point(257, 374);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(70, 70);
+            this.plusButton.TabIndex = 11;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = true;
+            this.plusButton.Click += new System.EventHandler(this.operator_click);
             // 
             // numberSix
             // 
@@ -181,67 +183,72 @@ namespace SchoolCalculator
             this.numberThree.UseVisualStyleBackColor = true;
             this.numberThree.Click += new System.EventHandler(this.button_click);
             // 
-            // button14
+            // equalButton
             // 
-            this.button14.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button14.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button14.Location = new System.Drawing.Point(333, 298);
-            this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(70, 146);
-            this.button14.TabIndex = 13;
-            this.button14.Text = "=";
-            this.button14.UseVisualStyleBackColor = false;
+            this.equalButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.equalButton.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equalButton.Location = new System.Drawing.Point(333, 298);
+            this.equalButton.Name = "equalButton";
+            this.equalButton.Size = new System.Drawing.Size(70, 146);
+            this.equalButton.TabIndex = 13;
+            this.equalButton.Text = "=";
+            this.equalButton.UseVisualStyleBackColor = false;
+            this.equalButton.Click += new System.EventHandler(this.equal_click);
             // 
-            // button15
+            // commaButton
             // 
-            this.button15.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button15.Location = new System.Drawing.Point(181, 374);
-            this.button15.Name = "button15";
-            this.button15.Size = new System.Drawing.Size(70, 70);
-            this.button15.TabIndex = 12;
-            this.button15.Text = ".";
-            this.button15.UseVisualStyleBackColor = true;
-            this.button15.Click += new System.EventHandler(this.button_click);
+            this.commaButton.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.commaButton.Location = new System.Drawing.Point(181, 374);
+            this.commaButton.Name = "commaButton";
+            this.commaButton.Size = new System.Drawing.Size(70, 70);
+            this.commaButton.TabIndex = 12;
+            this.commaButton.Text = ".";
+            this.commaButton.UseVisualStyleBackColor = true;
+            this.commaButton.Click += new System.EventHandler(this.button_click);
             // 
-            // button1
+            // minusButton
             // 
-            this.button1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(257, 298);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(70, 70);
-            this.button1.TabIndex = 15;
-            this.button1.Text = "-";
-            this.button1.UseVisualStyleBackColor = true;
+            this.minusButton.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusButton.Location = new System.Drawing.Point(257, 298);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(70, 70);
+            this.minusButton.TabIndex = 15;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = true;
+            this.minusButton.Click += new System.EventHandler(this.operator_click);
             // 
-            // button2
+            // multiButton
             // 
-            this.button2.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(257, 222);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(70, 70);
-            this.button2.TabIndex = 16;
-            this.button2.Text = "x";
-            this.button2.UseVisualStyleBackColor = true;
+            this.multiButton.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiButton.Location = new System.Drawing.Point(257, 222);
+            this.multiButton.Name = "multiButton";
+            this.multiButton.Size = new System.Drawing.Size(70, 70);
+            this.multiButton.TabIndex = 16;
+            this.multiButton.Text = "x";
+            this.multiButton.UseVisualStyleBackColor = true;
+            this.multiButton.Click += new System.EventHandler(this.operator_click);
             // 
-            // button3
+            // ceButton
             // 
-            this.button3.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(333, 149);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(70, 70);
-            this.button3.TabIndex = 17;
-            this.button3.Text = "CE";
-            this.button3.UseVisualStyleBackColor = true;
+            this.ceButton.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ceButton.Location = new System.Drawing.Point(333, 149);
+            this.ceButton.Name = "ceButton";
+            this.ceButton.Size = new System.Drawing.Size(70, 70);
+            this.ceButton.TabIndex = 17;
+            this.ceButton.Text = "CE";
+            this.ceButton.UseVisualStyleBackColor = true;
+            this.ceButton.Click += new System.EventHandler(this.clear_click);
             // 
-            // button5
+            // cButton
             // 
-            this.button5.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(333, 222);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(70, 70);
-            this.button5.TabIndex = 18;
-            this.button5.Text = "C";
-            this.button5.UseVisualStyleBackColor = true;
+            this.cButton.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cButton.Location = new System.Drawing.Point(333, 222);
+            this.cButton.Name = "cButton";
+            this.cButton.Size = new System.Drawing.Size(70, 70);
+            this.cButton.TabIndex = 18;
+            this.cButton.Text = "C";
+            this.cButton.UseVisualStyleBackColor = true;
+            this.cButton.Click += new System.EventHandler(this.c_click);
             // 
             // ResultBox
             // 
@@ -253,36 +260,35 @@ namespace SchoolCalculator
             this.ResultBox.Text = "0";
             this.ResultBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // label1
+            // showOperationLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.Control;
-            this.label1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(334, 49);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 20);
-            this.label1.TabIndex = 20;
-            this.label1.Text = "label1";
+            this.showOperationLabel.AutoSize = true;
+            this.showOperationLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.showOperationLabel.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showOperationLabel.Location = new System.Drawing.Point(390, 65);
+            this.showOperationLabel.Name = "showOperationLabel";
+            this.showOperationLabel.Size = new System.Drawing.Size(0, 20);
+            this.showOperationLabel.TabIndex = 20;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(442, 468);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showOperationLabel);
             this.Controls.Add(this.ResultBox);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cButton);
+            this.Controls.Add(this.ceButton);
+            this.Controls.Add(this.multiButton);
+            this.Controls.Add(this.minusButton);
             this.Controls.Add(this.numberThree);
-            this.Controls.Add(this.button14);
-            this.Controls.Add(this.button15);
-            this.Controls.Add(this.button8);
+            this.Controls.Add(this.equalButton);
+            this.Controls.Add(this.commaButton);
+            this.Controls.Add(this.plusButton);
             this.Controls.Add(this.numberSix);
             this.Controls.Add(this.numberFive);
             this.Controls.Add(this.numberFour);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.divButton);
             this.Controls.Add(this.numberNine);
             this.Controls.Add(this.numberEight);
             this.Controls.Add(this.numberSeven);
@@ -305,23 +311,23 @@ namespace SchoolCalculator
         private System.Windows.Forms.Button numberOne;
         private System.Windows.Forms.Button numberTwo;
         private System.Windows.Forms.Button numberZero;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button divButton;
         private System.Windows.Forms.Button numberNine;
         private System.Windows.Forms.Button numberEight;
         private System.Windows.Forms.Button numberSeven;
-        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button plusButton;
         private System.Windows.Forms.Button numberSix;
         private System.Windows.Forms.Button numberFive;
         private System.Windows.Forms.Button numberFour;
         private System.Windows.Forms.Button numberThree;
-        private System.Windows.Forms.Button button14;
-        private System.Windows.Forms.Button button15;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button equalButton;
+        private System.Windows.Forms.Button commaButton;
+        private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.Button multiButton;
+        private System.Windows.Forms.Button ceButton;
+        private System.Windows.Forms.Button cButton;
         private System.Windows.Forms.TextBox ResultBox;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label showOperationLabel;
     }
 }
 
--- SchoolCalculator/SchoolCalculator/Form1.cs
+++ SchoolCalculator/SchoolCalculator/Form1.cs
@@ -12,6 +12,10 @@ namespace SchoolCalculator
 {
     public partial class Form1 : Form
     {
+        Double valueResult = 0;
+        String operation = "";
+        bool operationPerformedCorrectly = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,11 +23,70 @@ namespace SchoolCalculator
 
         private void button_click(object sender, EventArgs e)
         {
-            if (ResultBox.Text=="0")
+            if ((ResultBox.Text=="0") || (operationPerformedCorrectly))
                 ResultBox.Clear();
 
+            operationPerformedCorrectly = false;
             Button button = (Button)sender;
+            if (button.Text == ".")
+            {
+                if (!ResultBox.Text.Contains("."))
+                    ResultBox.Text = ResultBox.Text + button.Text;
+            }
+            else
             ResultBox.Text = ResultBox.Text + button.Text;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (valueResult!= 0)
+            {
+                equalButton.PerformClick();
+                operation = button.Text;
+                showOperationLabel.Text = valueResult + " " + operation;
+                operationPerformedCorrectly = true;
+            }
+            else
+            {
+                operation = button.Text;
+                valueResult = Double.Parse(ResultBox.Text);
+                showOperationLabel.Text = valueResult + " " + operation;
+                operationPerformedCorrectly = true;
+            }
+            
+        }
+
+        private void clear_click(object sender, EventArgs e)
+        {
+            ResultBox.Text = "0";
+        }
+
+        private void c_click(object sender, EventArgs e)
+        {
+            ResultBox.Text = "0";
+            valueResult = 0;
+        }
+
+        private void equal_click(object sender, EventArgs e)
+        {
+            switch (operation)
+            {
+                case "+":
+                    ResultBox.Text = (valueResult + Double.Parse(ResultBox.Text)).ToString();
+                    break;
+                case "-":
+                    ResultBox.Text = (valueResult - Double.Parse(ResultBox.Text)).ToString();
+                    break;
+                case "x":
+                    ResultBox.Text = (valueResult * Double.Parse(ResultBox.Text)).ToString();
+                    break;
+                case "/":
+                    ResultBox.Text = (valueResult / Double.Parse(ResultBox.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
