b235324703a8ac7d933894254bf2d9539abc4826
Anna <anna.h.hallberg.92@gmail.com>
2020-12-13 12:58:10 +0100

    Comments

--- SchoolCalculator/SchoolCalculator/Form1.cs
+++ SchoolCalculator/SchoolCalculator/Form1.cs
@@ -12,15 +12,18 @@ namespace SchoolCalculator
 {
     public partial class Form1 : Form
     {
-        Double valueResult = 0;
-        String operation = "";
+        //Entry point
+        double valueResult = 0;
+        string operation = "";
         bool operationPerformedCorrectly = false;
+
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        //Functions for buttons for numbers and ","
         private void button_click(object sender, EventArgs e)
         {
             if ((ResultBox.Text=="0") || (operationPerformedCorrectly))
@@ -37,6 +40,7 @@ namespace SchoolCalculator
             ResultBox.Text = ResultBox.Text + button.Text;
         }
 
+        //Functions for buttons for "+" , "-" , "x" and "/"
         private void operator_click(object sender, EventArgs e)
         {
             Button button = (Button)sender;
@@ -50,45 +54,56 @@ namespace SchoolCalculator
             else
             {
                 operation = button.Text;
-                valueResult = Double.Parse(ResultBox.Text);
+                valueResult = double.Parse(ResultBox.Text);
                 showOperationLabel.Text = valueResult + " " + operation;
                 operationPerformedCorrectly = true;
             }
             
         }
 
+        //Function for button "CE"
         private void clear_click(object sender, EventArgs e)
         {
+
             ResultBox.Text = "0";
         }
 
+        //Function for button "C"
         private void c_click(object sender, EventArgs e)
         {
             ResultBox.Text = "0";
             valueResult = 0;
         }
 
+        //Function for button "="
         private void equal_click(object sender, EventArgs e)
         {
             switch (operation)
             {
                 case "+":
-                    ResultBox.Text = (valueResult + Double.Parse(ResultBox.Text)).ToString();
+                    ResultBox.Text = (valueResult + double.Parse(ResultBox.Text)).ToString();
                     break;
+
                 case "-":
-                    ResultBox.Text = (valueResult - Double.Parse(ResultBox.Text)).ToString();
+                    ResultBox.Text = (valueResult - double.Parse(ResultBox.Text)).ToString();
                     break;
+
                 case "x":
-                    ResultBox.Text = (valueResult * Double.Parse(ResultBox.Text)).ToString();
+                    ResultBox.Text = (valueResult * double.Parse(ResultBox.Text)).ToString();
                     break;
+
                 case "/":
-                    ResultBox.Text = (valueResult / Double.Parse(ResultBox.Text)).ToString();
+                    ResultBox.Text = (valueResult / double.Parse(ResultBox.Text)).ToString();
                     break;
+
                 default:
                     break;
             }
             valueResult = Double.Parse(ResultBox.Text);
             showOperationLabel.Text = "";
         }
+        
+
+
     }
 }
